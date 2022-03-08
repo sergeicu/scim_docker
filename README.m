5b8ce7f727dd9f0ea6ea12db8bd737b87f8165af
sergeicu <serge.vasylechko@gmail.com>
2022-03-08 13:06:51 -0500

    Update README.m

--- README.m
+++ README.m
@@ -9,83 +9,40 @@ build basic docker image based on centos6 with simon's tools and compile IVIM co
 This code is under [Apache 2.0](LICENSE.txt) licensing. \
 If you use it, please cite one of the following papers:
 
-**SynthSeg: Domain Randomisation for Segmentation of Brain MRI Scans of any Contrast and Resolution** \
-B. Billot, D.N. Greve, O. Puonti, A. Thielscher, K. Van Leemput, B. Fischl, A.V. Dalca, J.E. Iglesias \
-[[arxiv](https://arxiv.org/abs/2107.09559) | [bibtex](bibtex.bib)]
+**Reliable estimation of incoherent motion parametric maps from diffusion-weighted MRI using fusion bootstrap moves** \
+Freiman, Moti and Perez-Rossello, Jeannette M and Callahan, Michael J and Voss, Stephan D and Ecklund, Kirsten and Mulkern, Robert V and Warfield, Simon K \
+Medical Image Analysis 2013 \
+[[link](https://pubmed.ncbi.nlm.nih.gov/23434293/) | [bibtex](bibtex.bib)]
 
-**A Learning Strategy for Contrast-agnostic MRI Segmentation** \
-B. Billot, D.N. Greve, K. Van Leemput, B. Fischl, J.E. Iglesias*, A.V. Dalca* \
+
+**Spatially-constrained probability distribution model of incoherent motion (SPIM) for abdominal diffusion-weighted MRI** \
+Kurugol, Sila and Freiman, Moti and Afacan, Onur and Perez-Rossello, Jeannette M and Callahan, Michael J and Warfield, Simon K \
+Medical Image Analysis 2016 \
+[[link](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4903917/) | [bibtex](bibtex.bib)]
+
+
+**Spatially-constrained probability distribution model of incoherent motion (SPIM) in diffusion weighted MRI signals of crohn’s disease** \
+Kurugol, Sila and Freiman, Moti and Afacan, Onur and Perez-Rossello, Jeannette M and Callahan, Michael J and Warfield, Simon K \
+MICCAI 2014 \
+[[link](https://link.springer.com/chapter/10.1007/978-3-319-13692-9_11) | [bibtex](bibtex.bib)]
+
+
+**Evaluation of Motion-Compensated Spatially-Constrained IVIM (MC-SCIM) Model of Diffusion-weighted MRI for Assessment of Fibrosis in Crohn’s Disease using Surgical Histopathology Scores** \
+Kurugol, Sila and Freiman, Moti and Goldsmith, Jeffrey and Didier, Ryne and Afacan, Onur and Perez-Rossello, Jeanette M and Callahan, Michael J and Bousvaros, Athos and Warfield, Simon K \
+ISMRM 2017 \
+[[link](https://scholar.harvard.edu/silakurugol/publications/evaluation-motion-compensated-spatially-constrained-ivim-mc-scim-model) | [bibtex](bibtex.bib)]
+
+
+**Self-supervised IVIM DWI parameter estimation with a physics based forward model** \
+Vasylechko, Serge Didenko and Warfield, Simon K and Afacan, Onur* and Kurugol, Sila* \
 *contributed equally \
-MIDL 2020 \
-[[link](http://proceedings.mlr.press/v121/billot20a.html) | [arxiv](https://arxiv.org/abs/2003.01995) | [bibtex](bibtex.bib)]
-
-**Partial Volume Segmentation of Brain MRI Scans of any Resolution and Contrast** \
-B. Billot, E.D. Robinson, A.V. Dalca, J.E. Iglesias \
-MICCAI 2020 \
-[[link](https://link.springer.com/chapter/10.1007/978-3-030-59728-3_18) | [arxiv](https://arxiv.org/abs/2004.10221) | [bibtex](bibtex.bib)]
+MRM 2022 \
+[[link](https://onlinelibrary.wiley.com/doi/10.1002/mrm.28989) | [arxiv](https://arxiv.org) | [bibtex](bibtex.bib)]
 
 
-@article{freiman2013reliable,
-  title={Reliable estimation of incoherent motion parametric maps from diffusion-weighted MRI using fusion bootstrap moves},
-  author={Freiman, Moti and Perez-Rossello, Jeannette M and Callahan, Michael J and Voss, Stephan D and Ecklund, Kirsten and Mulkern, Robert V and Warfield, Simon K},
-  journal={Medical image analysis},
-  volume={17},
-  number={3},
-  pages={325--336},
-  year={2013},
-  publisher={Elsevier}
-}
-
-@article{kurugol2016spatially,
-  title={Spatially-constrained probability distribution model of incoherent motion (SPIM) for abdominal diffusion-weighted MRI},
-  author={Kurugol, Sila and Freiman, Moti and Afacan, Onur and Perez-Rossello, Jeannette M and Callahan, Michael J and Warfield, Simon K},
-  journal={Medical image analysis},
-  volume={32},
-  pages={173--183},
-  year={2016},
-  publisher={Elsevier}
-}
-
-@inproceedings{kurugol2014spatially,
-  title={Spatially-constrained probability distribution model of incoherent motion (SPIM) in diffusion weighted MRI signals of crohn’s disease},
-  author={Kurugol, Sila and Freiman, Moti and Afacan, Onur and Perez-Rossello, Jeannette M and Callahan, Michael J and Warfield, Simon K},
-  booktitle={International MICCAI Workshop on Computational and Clinical Challenges in Abdominal Imaging},
-  pages={117--127},
-  year={2014},
-  organization={Springer}
-}
-
-@article{kurugolevaluation,
-  title={Evaluation of Motion-Compensated Spatially-Constrained IVIM (MC-SCIM) Model of Diffusion-weighted MRI for Assessment of Fibrosis in Crohn’s Disease using Surgical Histopathology Scores},
-  author={Kurugol, Sila and Freiman, Moti and Goldsmith, Jeffrey and Didier, Ryne and Afacan, Onur and Perez-Rossello, Jeanette M and Callahan, Michael J and Bousvaros, Athos and Warfield, Simon K}
-}
-
-@article{vasylechko2022self,
-  title={Self-supervised IVIM DWI parameter estimation with a physics based forward model},
-  author={Vasylechko, Serge Didenko and Warfield, Simon K and Afacan, Onur and Kurugol, Sila},
-  journal={Magnetic Resonance in Medicine},
-  volume={87},
-  number={2},
-  pages={904--914},
-  year={2022},
-  publisher={Wiley Online Library}
-}
 
 If you have any question regarding the usage of this code, or any suggestions to improve it, you can contact us at: \
 serge.vasylechko@childrens.harvard.edu
-
-----------------
-
-### References
-
-[1] *[Anatomical Priors in Convolutional Networks for Unsupervised Biomedical Segmentation](http://www.mit.edu/~adalca/files/papers/cvpr2018_priors.pdf)* \
-Adrian V. Dalca, John Guttag, Mert R. Sabuncu \
-CVPR 2018
-
-[2] *[Unsupervised Data Imputation via Variational Inference of Deep Subspaces](https://arxiv.org/abs/1903.03503)* \
-Adrian V. Dalca, John Guttag, Mert R. Sabuncu \
-Arxiv preprint 2019
-                                                                                
-                                                                                
+                                                                     
      
 ```
